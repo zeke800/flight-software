709b25f9161ef7b781acc43e0d6cd45be3845cf8
zeke800 <82127189+zeke800@users.noreply.github.com>
2022-08-04 11:37:15 -0400

    Update pic_downlink.py

--- pic_downlink.py
+++ pic_downlink.py
@@ -1,5 +1,6 @@
 import cv2
 from PIL import Image
+import os
 #Camera settings
 camera_id = 0
 cap = cv2.VideoCapture(camera_id)
@@ -8,6 +9,11 @@ image_path = "C:\\Users\\sasha\\Documents\\Flight Software"
 width = 320 #As per SSDV standards, height must be a multiple of 16
 height = 320 #As per SSDV standards, width must be a multiple of 16
 
+#Various downlink settings
+jpg_quality = 100
+callsign = "ZEKE800" #Yea change that to your callsign
+ssdv_path = "C:\\Users\\sasha\\Documents\\Flight Software\\ssdv"
+binary_path = "C:\\Users\\sasha\\Documents\\Flight Software\\ssdv\\packets"
 def take_pic():
     global image_id
     ret, frame = cap.read()
@@ -23,7 +29,17 @@ def release_camera():
     cap.release()
 
 def process_image(img):
-    global im
     im = Image.open(img)
     im = im.resize((width,height),Image.ANTIALIAS)
     im.save(img)
+    return img
+
+def convert2jpg(img):
+    im = Image.open(img)
+    im.save(img[0:len(img)-3]+"jpg",quality=jpg_quality) #Magic to convert to jpg :)
+    #Removes the last three characters (png) and replaces with jpg
+
+def convert2ssdv(img):
+    print("Convert picture into SSDV [ID=" + str(image_id) + "]")
+    os.system(ssdv_path+'/ssdv -e -t ' + str(jpg_quality) + ' -c ' + callsign + ' -i ' + str(image_id) + str(img)+ binary_path+'/image.ssdv')
+    print("Done encoding SSDV [ID="+ str(image_id) +"]")
