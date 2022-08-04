f69ff318a65da5d7efc56e6e17ba53c145480009
zeke800 <82127189+zeke800@users.noreply.github.com>
2022-08-04 09:15:21 -0400

    Update pic_downlink.py

--- pic_downlink.py
+++ pic_downlink.py
@@ -1,16 +1,29 @@
 import cv2
-
+from PIL import Image
 #Camera settings
 camera_id = 0
 cap = cv2.VideoCapture(camera_id)
 image_id = 0
-image_path = ""
-ret, frame = cap.read()
+image_path = "C:\\Users\\sasha\\Documents\\Flight Software"
+width = 320 #As per SSDV standards, height must be a multiple of 16
+height = 320 #As per SSDV standards, width must be a multiple of 16
 
 def take_pic():
+    global image_id
+    ret, frame = cap.read()
     grabbed, frame = cap.read()
-    image = image_path+"/"+image_id+".jpg"
+    image = image_path+"/"+str(image_id)+".png"
+    #Avoid saving jpgs until the very last process (ssdv encode) which requires
+    #JPEG as a format
     cv2.imwrite(image, frame)
-    cap.release()
     image_id = image_id + 1
     return image
+def release_camera():
+    global cap
+    cap.release()
+
+def process_image(img):
+    global im
+    im = Image.open(img)
+    im = im.resize((width,height),Image.ANTIALIAS)
+    im.save(img)
